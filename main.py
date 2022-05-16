722b46fc284e1318f3a58627df86a0809b97237f
Alex <alexss81@mail.ru>
2022-05-16 21:49:38 +0300

    Create main.py file

---
+++ main.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+
+def get_html():
+    pass
+
+
+def get_data():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
