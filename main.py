1e2da00ebdc49f62f8baa1f013686c7f835bdd46
Alex <alexss81@mail.ru>
2022-05-22 23:37:41 +0300

    modified main.py

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 
+# Getting a car's VIN by number
 def get_vin():
     number = input("Enter the car number: ")
     time.sleep(3)
@@ -30,6 +32,7 @@ def get_vin():
     return vin
 
 
+# Checking the registration history in the traffic police
 def get_history(vin):
     driver = webdriver.Chrome(executable_path="Path")
 
@@ -38,14 +41,13 @@ def get_history(vin):
 
     elem = driver.find_element_by_id('checkAutoVIN')
     elem.send_keys(vin)
-    share = driver.find_element_by_class_name('checker')
-    share.click()
+    driver.find_element_by_class_name('checker').click()
 
     try:
         WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, 'close_modal_window'))).click()
         time.sleep(15)
-    except:
-        print("Error")
+    except TimeoutException:
+        print("TimeoutException")
 
     html = driver.page_source
     soup = BeautifulSoup(html, "lxml")
@@ -73,15 +75,90 @@ def get_history(vin):
         'Периоды владения тс': owner_list,
     }
 
-    with open('data.json', "w", encoding="utf-8") as file:
+    with open('data_history.json', "w", encoding="utf-8") as file:
         json.dump(data, file, indent=4, ensure_ascii=False)
 
-def get_pdd():
-    pass
+
+# Checking for participation in road accidents
+def get_dtp(vin):
+    driver = webdriver.Chrome(executable_path=
+                              "C:\\Users\\vovik\\PycharmProjects\\test\\Sel\\ChromeDriver\\chromedriver.exe")
+
+    driver.get('https://xn--90adear.xn--p1ai/check/auto')
+    time.sleep(30)
+
+    elem = driver.find_element_by_id('checkAutoVIN')
+    elem.send_keys(vin)
+    driver.find_element_by_xpath("//a[@class='checker'][@data-type='aiusdtp']").click()
+
+    try:
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CLASS_NAME, 'close_modal_window'))).click()
+    except TimeoutException:
+        print("TimeoutException")
+
+    html = driver.page_source
+    soup = BeautifulSoup(html, "lxml")
+
+    cards = soup.find("ul", class_="aiusdtp-list").find_all("li")[::17]
+
+    data = []
+    for i in cards:
+        try:
+            title = i.find("p", class_="ul-title").text.strip()
+        except:
+            title = ''
+        try:
+            data_time = i.find_all("li")[0].find("span", class_="field").text.strip()
+        except:
+            data_time = ""
+        try:
+            type_dtp = i.find_all("li")[1].find("span", class_="field").text.strip()
+        except:
+            type_dtp = ''
+        try:
+            region = i.find_all("li")[2].find("span", class_="field").text.strip()
+        except:
+            region = ''
+        try:
+            place = i.find_all("li")[3].find("span", class_="field").text.strip()
+        except:
+            place = ''
+        try:
+            model = i.find_all("li")[4].find("span", class_="field").text.strip()
+        except:
+            model = ''
+        try:
+            year = i.find_all("li")[5].find("span", class_="field").text.strip()
+        except:
+            year = ''
+        try:
+            owner = i.find_all("li")[6].find("span", class_="field").text.strip()
+        except:
+            owner = ''
+        try:
+            number = i.find_all("li")[7].find("span", class_="field").text.strip()
+        except:
+            number = ''
+
+        data.append({
+                "Информация о происшествии": title,
+                "Дата и время происшествия:": data_time,
+                "Тип происшествия:": type_dtp,
+                "Регион происшествия:": region,
+                "Место происшествия:": place,
+                "Марка (модель) ТС:": model,
+                "Год выпуска ТС:": year,
+                "ОПФ собственника:": owner,
+                "Номер ТС/из всего ТС в ДТП:": number,
+            })
+    with open('data_dtp.json', "w", encoding="utf-8") as file:
+        json.dump(data, file, indent=4, ensure_ascii=False)
+
 
 def main():
     vin = get_vin()
     get_history(vin)
+    get_dtp(vin)
 
 
 if __name__ == '__main__':
